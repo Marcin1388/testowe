ae0f80e0af9c1da24ad1afc0dd135a30e64ea502
Marcin P <marcineek1988@gmail.com>
2016-09-08 19:25:40 +0200

    lista gostkow + pokazywanie bottom-sheet - ale nie odswieza na nim danych wybranego usera

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -4,9 +4,6 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create(); //auto-odswiezanie przegladarki
 
-gulp.task('hello', function () {
-    console.log('Hello world ! :)');
-});
 
 gulp.task('browserSync', function () {
     browserSync.init({
--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,62 @@
-var app = angular.module('start', ['ngMaterial']);
+angular.module('start', ['ngMaterial'])
+    .config(function ($mdIconProvider) {
+        $mdIconProvider.defaultIconSet('./img/svg/avatars.svg', 128);
+        $mdIconProvider.icon('share', './img/svg/share.svg', 24);
+    })
+    .controller('firstCtrl', function ($mdBottomSheet) {
+        this.tekst = "It's Working ! :)";
 
-app.controller('firstCtrl', function ($scope) {
-    $scope.tekst = "It's Working ! :)";
+        this.users = [
+            {
+                name: 'Marcin',
+                imgPath: 'svg-1',
+                content: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et dolor nec magna elementum ornare. Phasellus volutpat nibh porta, dignissim nisl vel, volutpat nibh. Morbi sit amet sem sed leo bibendum consequat nec vel dui. Nulla facilisi. In in lorem ac quam auctor imperdiet eu id velit. Proin eu congue est. Quisque non metus vestibulum arcu commodo sagittis eget a purus. Duis tincidunt erat in odio sollicitudin pellentesque. Donec ornare ultricies ultrices. Cras sollicitudin porttitor leo, a imperdiet dui iaculis eget. Aliquam erat volutpat. Duis convallis blandit volutpat. Sed eget pellentesque ipsum. Suspendisse scelerisque nec dolor in pellentesque. '
+            },
+            {
+                name: 'Janek',
+                imgPath: 'svg-10',
+                content: ' Aliquam congue efficitur dui, ac volutpat mi semper a. Nam scelerisque condimentum suscipit. Nunc malesuada eu purus quis congue. Cras mi urna, accumsan a neque non, interdum ornare neque. Donec orci eros, lacinia non sodales eget, accumsan ac metus. Etiam sem tellus, ornare pretium feugiat quis, finibus non magna. Suspendisse vel augue non risus tempor bibendum. Vestibulum sit amet nibh faucibus, finibus dolor nec, feugiat dui. Nunc sed ultrices nunc, vel bibendum nisl. Nam dapibus gravida eros in consectetur. Maecenas pulvinar a mauris vitae aliquam. Ut vestibulum augue vel nisl eleifend varius. Etiam ullamcorper at tortor id hendrerit. Nullam malesuada tincidunt nibh, vitae varius ante. Fusce vitae rutrum neque. Proin dui est, malesuada at lectus ac, consectetur faucibus erat. '
+            },
+            {
+                name: 'Eryk',
+                imgPath: 'svg-11',
+                content: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et dolor nec magna elementum ornare. Phasellus volutpat nibh porta, dignissim nisl vel, volutpat nibh. Morbi sit amet sem sed leo bibendum consequat nec vel dui. Nulla facilisi. In in lorem ac quam auctor imperdiet eu id velit. Proin eu congue est. Quisque non metus vestibulum arcu commodo sagittis eget a purus. Duis tincidunt erat in odio sollicitudin pellentesque. Donec ornare ultricies ultrices. Cras sollicitudin porttitor leo, a imperdiet dui iaculis eget. Aliquam erat volutpat. Duis convallis blandit volutpat. Sed eget pellentesque ipsum. Suspendisse scelerisque nec dolor in pellentesque. '
+            }
+        ];
 
-    $scope.users = [
-        {
-            name : 'Marcin',
-            imgPath : 'img/svg/avatar-1.svg',
-            content : ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et dolor nec magna elementum ornare. Phasellus volutpat nibh porta, dignissim nisl vel, volutpat nibh. Morbi sit amet sem sed leo bibendum consequat nec vel dui. Nulla facilisi. In in lorem ac quam auctor imperdiet eu id velit. Proin eu congue est. Quisque non metus vestibulum arcu commodo sagittis eget a purus. Duis tincidunt erat in odio sollicitudin pellentesque. Donec ornare ultricies ultrices. Cras sollicitudin porttitor leo, a imperdiet dui iaculis eget. Aliquam erat volutpat. Duis convallis blandit volutpat. Sed eget pellentesque ipsum. Suspendisse scelerisque nec dolor in pellentesque. '
-        },
-        {
-            name : 'Janek',
-            imgPath : 'img/svg/avatar-4.svg',
-            content : ' Aliquam congue efficitur dui, ac volutpat mi semper a. Nam scelerisque condimentum suscipit. Nunc malesuada eu purus quis congue. Cras mi urna, accumsan a neque non, interdum ornare neque. Donec orci eros, lacinia non sodales eget, accumsan ac metus. Etiam sem tellus, ornare pretium feugiat quis, finibus non magna. Suspendisse vel augue non risus tempor bibendum. Vestibulum sit amet nibh faucibus, finibus dolor nec, feugiat dui. Nunc sed ultrices nunc, vel bibendum nisl. Nam dapibus gravida eros in consectetur. Maecenas pulvinar a mauris vitae aliquam. Ut vestibulum augue vel nisl eleifend varius. Etiam ullamcorper at tortor id hendrerit. Nullam malesuada tincidunt nibh, vitae varius ante. Fusce vitae rutrum neque. Proin dui est, malesuada at lectus ac, consectetur faucibus erat. '
-        }
-    ];
+        this.currUser = this.users[0];
 
+        this.setCurrentUser = function(user) {
+            this.currUser = user;
+            console.log('CURRENT: ' + this.currUser.name);
+        };
 
-});
+        this.getCurrentUser = function() {
+            return this.currUser;
+        };
+
+        this.openBottomSheet = function(user) {
+            console.log('CURRENT BOTTOM: ' + this.currUser.name);
+            $mdBottomSheet.show({
+                controller : "firstCtrl",
+                controllerAs : "fCtrl",
+                templateUrl: "userTmp.html",
+                $viewChangeListeners : true,
+                parent : angular.element(document.querySelector('#content'))
+            }).then(function () {
+                this.alert('kliknieto :)');
+            });
+        };
+
+    });
+
+    // .directive('showUserInformation', function () {
+    //    return {
+    //        restrict : 'AE',
+    //        scope : {
+    //            user : '='
+    //        },
+    //        templateUrl : 'userTmp.html'
+    //        // template: "<md-bottom-sheet></md-bottom-sheet><div><h1>{{user.name}}</h1> <p>{{user.content}}</p></div></md-bottom-sheet>"
+    //    }
+    // });
