4f8e43e70801ab0f021b91fd482c6ffdd7572c70
Marcin P <marcineek1988@gmail.com>
2016-09-15 19:59:57 +0200

    usuniecie -detailsController - juz nie potrzebny

--- src/controllers/detailController.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by marcinp on 14.09.16.
- */
-app.controller('detailController', function ($routeParams, mountainList) {
-    this.detail = mountainList.getById($routeParams.id);
-    // this.detail = mountainList.getById(2);
-});
